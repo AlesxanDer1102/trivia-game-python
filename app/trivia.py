c6715af59967bd10de8ed478273b4963b46aa442
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-04-12 11:16:18 -0500

    Implementación del sistema de puntuacion, manejo de rondas y finalizacion del juego

--- app/trivia.py
+++ app/trivia.py
@@ -12,6 +12,8 @@ class Quiz:
     def __init__(self):
         self.questions = []
         self.current_question_index = 0
+        self.correct_answers = 0
+        self.incorrect_answers = 0
         
 
     def add_question(self, question):
@@ -23,6 +25,14 @@ class Quiz:
             self.current_question_index += 1
             return question
         return None
+    
+    def answer_question(self,question,answer):
+        if question.is_correct(answer):
+            self.correct_answers += 1
+            return True
+        else:
+            self.incorrect_answers += 1
+            return False
     
 
 def run_quiz():
@@ -36,9 +46,26 @@ def run_quiz():
 
     preg1 = Question("¿Cuál es la capital de Peru?", ["Cali", "Bogota", "Lima", "Santiago"], "Lima")
     preg2 = Question("¿Cuál es la capital de Francia?", ["Londres", "Berlín", "Madrid", "París"], "París")
+    preg3 = Question("¿Cuál es la capital de España?", ["Londres", "Berlín", "Madrid", "París"], "Madrid")
+    preg4 = Question("¿Cuál es la capital de Alemania?", ["Londres", "Berlín", "Madrid", "París"], "Berlín")
+    preg5 = Question("¿Cuál es la capital de Inglaterra?", ["Londres", "Berlín", "Madrid", "París"], "Londres")
+    preg6 = Question("¿Cuál es la capital de Chile?", ["Londres", "Berlín", "Santiago", "París"], "Santiago")
+    preg7 = Question("¿Cuál es la capital de Argentina?", ["Londres", "Buenos Aires", "Madrid", "París"], "Buenos Aires")
+    preg8 = Question("¿Cuál es la capital de Colombia?", ["Cali", "Bogota", "Lima", "Santiago"], "Bogotá")
+    preg9 = Question("¿Cuál es la capital de Ecuador?", ["Cali", "Bogota", "Lima", "Quito"], "Quito")
+    preg10 = Question("¿Cuál es la capital de Venezuela?", ["Caracas", "Bogota", "Lima", "Santiago"], "Caracas")
+
 
     q.add_question(preg1)
     q.add_question(preg2)
+    q.add_question(preg3)
+    q.add_question(preg4)
+    q.add_question(preg5)
+    q.add_question(preg6)
+    q.add_question(preg7)
+    q.add_question(preg8)
+    q.add_question(preg9)
+    q.add_question(preg10)
 
     # Recorrer a travez de las preguntas del quiz
 
